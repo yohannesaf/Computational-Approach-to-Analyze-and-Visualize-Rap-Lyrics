45c947c18a267327f3ddd47c543e51a551a420bb
Yohannes <yohannes.af@gmail.com>
2016-11-12 11:48:46 -0800

    Functions to read the file, tokenize and return a tuple of word and its phonetic sounds

---
+++ Syllables.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import pronouncing as pr
+from nltk.tokenize import word_tokenize
+from nltk.corpus import cmudict
+from collections import OrderedDict
+
+def read_tokenize_file(filepath):
+    '''
+    Returns 2 - d lists of tokenize lyrics
+    '''
+    content = []
+    with open(filepath) as f:
+        for line in f.readlines():
+            content.append(word_tokenize(line.lower().strip()))
+    return content
+
+
+def word_aphabet_dict(string):
+    '''
+    Input: 2 - d list of tokenied workds
+    Output: Ordered dictionary:
+        Keys - word
+        Value - phonetic representation of the key
+    '''
+    aphabet = cmudict.dict()
+    word_phonetic = OrderedDict()
+    # for line in string:
+    print line
+    print '\n***************\n'
+    for word in string:
+        try:
+            # print word
+            word_phonetic.update({word:aphabet[word][0]})
+        except Exception as e:
+            print e
+    return word_phonetic
+
+
+def word_aphabet_tuple(string):
+    '''
+    Input: 2 - d list of tokenied workds
+    Output: Tuple:
+        Keys - word
+        Value - phonetic representation of the key
+    '''
+    aphabet = cmudict.dict()
+    output = []
+    for line in string:
+        for  word in line:
+            try:
+                output.append((word, aphabet[word][0]))
+            except Exception as e:
+                print e
+    return output
+
+# output = word_aphabet(testing)
+# print output
+
+
+
+
+if __name__ == '__main__':
+    verse = read_tokenize_file('/home/yoyo/Desktop/mini.txt')
+    # for line in verse:
+        # print verse
+        # print '\n******2******\n'
+    phonetics = word_aphabet_tuple(verse)
+    print phonetics
