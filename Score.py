1c04ba5bcefb6dd6aad0bf3a8a3b9d72576de5c6
Yohannes <yohannes.af@gmail.com>
2016-11-15 13:48:56 -0800

    matrix

--- Score.py
+++ Score.py
@@ -1,6 +1,22 @@
 from PrepareText import PrepareText
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from itertools import combinations
+
+
+text = PrepareText()
+text.read_tokenize_file('lyrics/forgot.md')
+text.word_aphabet_dict()
+text.clean_syllables()
+
+def similarity():
+    mat = []
+    for w1, w2 in combinations(text.syllable_dict.values(), 2):
+        for syl1 in w1:
+            for syl2 in w2:
+                # score(SOMETHING)
+                return mat[syl1][syl2] = 1
 
 def prefix_score():
     pass
@@ -10,6 +26,9 @@ def suffix_score():
 
 def vowel_score():
     pass
+
+def sound_intersect(phonetic1, phonetic2):
+    return list(set(a) & set(b))
 
 def is_vowel(char):
     '''
@@ -27,7 +46,15 @@ def is_num(char = 'x'):
 
 
 if __name__ == '__main__':
-    text = PrepareText()
-    text.read_tokenize_file('lyrics/forgot.md')
-    text.word_aphabet_dict()
-    text.clean_syllables()
+    test = similarity(), 'check'
+    print test
+
+# text = PrepareText()
+# text.read_tokenize_file('lyrics/forgot.md')
+# text.word_aphabet_dict()
+# text.clean_syllables()
+#
+# cv = CountVectorizer()
+# cv_fit = cv.fit_transform(text.aphabet.values())
+# print cv_fit.toarray
+#
