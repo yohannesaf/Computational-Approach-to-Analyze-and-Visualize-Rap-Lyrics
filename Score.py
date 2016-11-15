65587f9b4b949a2a66155545e2ae2dcfe61b88c5
Yohannes <yohannes.af@gmail.com>
2016-11-15 15:54:11 -0800

    updates

--- Score.py
+++ Score.py
@@ -10,13 +10,49 @@ text.read_tokenize_file('lyrics/forgot.md')
 text.word_aphabet_dict()
 text.clean_syllables()
 
-def similarity():
-    mat = []
+
+
+def similarity_mat():
+    temp = text.syllable_dict.values()
+    n = score_size()
+    col = []
+    row = []
+    score = np.zeros((n,n))
+    for word1 in text.syllable_dict.values():
+        col_ind = 0
+        for syl1 in word1:
+            col.append(syl1)
+            row_ind = 0
+            for word2 in temp:
+                if word1 != word2:
+                    for syl2 in word2:
+                        row.append(syl2)
+                        score[row_ind][col_ind] = 1
+                else:
+                    for syl2 in word2:
+                        row.append(syl2)
+                        score[row_ind][col_ind] = 0
+            row_ind += 1
+        col_ind +=1
+    return score, col, row
+
+
+def similarity1():
+    score = []
+    col = []
+    row = []
     for w1, w2 in combinations(text.syllable_dict.values(), 2):
         for syl1 in w1:
+            col.append(syl1)
             for syl2 in w2:
-                # score(SOMETHING)
-                return mat[syl1][syl2] = 1
+                row.append(syl2)
+                score.append(1)
+    return score, col, row
+
+def score_size():
+    size = 0
+    for word in text.syllable_dict.values():
+        size += len(word)
 
 def prefix_score():
     pass
@@ -46,8 +82,8 @@ def is_num(char = 'x'):
 
 
 if __name__ == '__main__':
-    test = similarity(), 'check'
-    print test
+    score, col, row = similarity1()
+    print score
 
 # text = PrepareText()
 # text.read_tokenize_file('lyrics/forgot.md')
