0a05ba2166b246be5bedb4e00d5f03edcf60b15e
Yohannes <yohannes.af@gmail.com>
2016-11-16 09:56:10 -0800

    updates

--- Score.py
+++ Score.py
@@ -5,52 +5,26 @@ from sklearn.feature_extraction.text import CountVectorizer
 from itertools import combinations
 import copy
 
-
 text = PrepareText()
 text.read_tokenize_file('lyrics/forgot.md')
 text.word_aphabet_dict()
 text.clean_syllables()
 
-# def similarity_mat():
-#     temp = text.syllable_dict.values()
-#     n = score_size()
-#     col = []
-#     row = []
-#     score = np.zeros((n,n))
-#     print score.shape
-#     for word1 in text.syllable_dict.values():
-#         col_ind = 0
-#         for syl1 in word1:
-#             col.append(syl1)
-#             row_ind = 0
-#             for word2 in temp:
-#                 if word1 != word2:
-#                     for syl2 in word2:
-#                         row.append(syl2)
-#                         score[row_ind][col_ind] = 1
-#                 else:
-#                     for syl2 in word2:
-#                         row.append(syl2)
-#                         score[row_ind][col_ind] = 0
-#             row_ind += 1
-#         col_ind +=1
-#     return score, col, row
-
 def similarity_mat():
-    syl_list1 = syllable_list()
+    syl_list1 = syllable_list(syllable_dict)
     syl_list2 = copy.deepcopy(syl_list1)
-    score = score_size()
+    score = score_size(syllable_dict)
     for ind1, val1 in enumerate(syl_list1):
         for ind2, val2 in enumerate(syl_list2):
             if val1 != val2:
                 common_sound = sound_intersect(val1, val2)
                 if len(common_sound) > 0:
-                    # points = final_score(val1, val2, common_sound)
-                    score[ind1][ind2] = 1
+                    points = final_score(val1, val2, common_sound)
+                    score[ind1][ind2] = points
                 else:
                     score[ind1][ind2] = 0
             else:
-                score[ind1][ind2] = -11
+                score[ind1][ind2] = np.nan
     return score, syl_list1
 
 # Update the input function
@@ -67,9 +41,9 @@ def syllable_list(syllable_dict):
         syl_list.extend(val)
     return syl_list
 
-def score_size():
+def score_size(syllable_dict):
     size = 0
-    for word in text.syllable_dict.values():
+    for word in syllable_dict.itervalues():
         size += len(word)
     return np.zeros((size,size))
 
@@ -89,7 +63,7 @@ def final_score(phonetic1, phonetic2, common_sounds):
             points += consonant_score(phonetic1, phonetic2, sound)
 
 
-def consonant_score(sound):
+def consonant_score(phonetic1, phonetic2, sound):
     '''
     Input: Consonant phone
     Output: Consonant phone score
@@ -100,9 +74,10 @@ def consonant_score(sound):
     ind1 = phonetic1.index(sound)
     ind2 = phonetic2.index(sound)
     if ind1 < ind2:
-        return points += 1
+        points += 1
     else:
-        return points += 2
+        points += 2
+    return points
 
 def vowel_score(sound):
     '''
@@ -111,10 +86,10 @@ def vowel_score(sound):
 
     Assigns score to a common phone vowel bewteen 2 words based on their stress.
     '''
-    if syllable[-1] > 0:
-        return len(syllable) * 3
+    if sound[-1] > 0:
+        return len(sound) * 3
     else:
-        return len(syllable) + 2
+        return len(sound) + 2
 
 def sound_intersect(phonetic1, phonetic2):
     '''
@@ -130,26 +105,13 @@ def is_vowel(text):
     Returns true if a character is a vowel
     '''
     vowels = 'aeiouAEIOU'
-    return [True if char in vowels for char in text]
+    return [True if char in vowels else False for char in text]
 
 def is_num(char = 'x'):
     '''
     Returns true if a character is a number
     '''
     return char.isdigit()
-
-# def similarity1():
-#     score = []
-#     col = []
-#     row = []
-#     for w1, w2 in combinations(text.syllable_dict.values(), 2):
-#         for syl1 in w1:
-#             col.append(syl1)
-#             for syl2 in w2:
-#                 row.append(syl2)
-#                 print syl1, syl2
-#                 score.append(1)
-#     return score, col, row
 
 
 
