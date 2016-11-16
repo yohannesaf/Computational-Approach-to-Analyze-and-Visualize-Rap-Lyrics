4035c33342b640d8a432f58d5f0ab6acfa47f8f9
Yohannes <yohannes.af@gmail.com>
2016-11-15 17:28:29 -0800

    similarity matrix construction works. next, implement scoring.

--- Score.py
+++ Score.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from itertools import combinations
+import copy
 
 
 text = PrepareText()
@@ -37,16 +38,23 @@ text.clean_syllables()
 
 def similarity_mat():
     syl_list1 = syllable_list()
-    syl_list2 = syllable_list()
+    syl_list2 = copy.deepcopy(syl_list1)
     size = len(syl_list1)
     score = score_size()
     for ind1, val1 in enumerate(syl_list1):
         for ind2, val2 in enumerate(syl_list2):
-            print (ind1, ind2)
+            # print (ind1, ind2)
             if val1 != val2:
-                score[ind1][ind2] = 1
+                common_sound = sound_intersect(val1, val2)
+                if len(common_sound) > 0:
+                    # pre_score = SOMETHING
+                    # suf_score = SOMETHING
+                    # Vowel_score = SOMETHING
+                    score[ind1][ind2] = 1
+                else:
+                    score[ind1][ind2] = 0
             else:
-                score[ind1][ind2] = 1
+                score[ind1][ind2] = -1
     return score, syl_list1
 
 def syllable_list():
@@ -61,6 +69,9 @@ def score_size():
         size += len(word)
     return np.zeros((size,size))
 
+def final_score(common_sounds):
+    pass
+
 def prefix_score():
     pass
 
@@ -71,7 +82,7 @@ def vowel_score():
     pass
 
 def sound_intersect(phonetic1, phonetic2):
-    return list(set(a) & set(b))
+    return list(set(phonetic1) & set(phonetic2))
 
 def is_vowel(char):
     '''
@@ -103,7 +114,7 @@ def is_num(char = 'x'):
 
 if __name__ == '__main__':
     score, col = similarity_mat()
-    # print score
+    print score
 
 # text = PrepareText()
 # text.read_tokenize_file('lyrics/forgot.md')
