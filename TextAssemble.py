c50a52cc56b50144857fd4beb8f577584b51ee16
Yohannes <yohannes.af@gmail.com>
2016-11-20 15:52:28 -0800

    updates

--- TextAssemble.py
+++ TextAssemble.py
@@ -18,12 +18,19 @@ class TextAssemble(ScoreMechanism):
         ScoreMechanism.__init__(self, filepath)
         self.M_output = None
         self.clusters = defaultdict()
-        self.clustered_syl = OrderedDict()
+        # self.clustered_syl = OrderedDict()
+        self.clustered_syl = []
+        self.grouped_syl = OrderedDict()
 
         self.mcl_cluster()
         self.phone_name_assignment()
+        self.syl_combine()
+
 
     def mcl_cluster(self):
+        '''
+        Computes Marcov Cluster Algorithm
+        '''
         self.M_output, self.clusters = mcl(self.adjacency_matrix,
                           expand_factor = 7,
                           inflate_factor = 3,
@@ -31,11 +38,15 @@ class TextAssemble(ScoreMechanism):
                           mult_factor = 4
                           )
 
-    # print score.phone_syl_col
     def phone_name_assignment(self):
+        '''
+        Assigns the word syllables the approprate cluster
+        '''
         temp = self.cluster_val_inversion()
+        # test = []
         for syl, cl in temp.iteritems():
-            self.clustered_syl.update({self.word_syl_col[syl]:cl})
+            self.clustered_syl.append([self.word_syl_col[syl], cl])
+            # self.clustered_syl.update({self.word_syl_col[syl]:cl})
 
     def cluster_val_inversion(self):
         '''
@@ -50,6 +61,22 @@ class TextAssemble(ScoreMechanism):
             else:
                 temp.update({syl[0]:99})
         return temp
+
+    # Not working at the moment
+    def syl_combine(self):
+        word_ind = 0
+        syl_counts = self.word_syl_count()
+        for key, syl_len in syl_counts.iteritems():
+            temp = self.clustered_syl[word_ind:word_ind+syl_len]
+            self.grouped_syl.update({key:temp})
+            word_ind += syl_len
+
+
+    def word_syl_count(self):
+        syl_counts = OrderedDict()
+        for key, val in self.phonetic_syl_dict.iteritems():
+            syl_counts.update({key:len(val)})
+        return syl_counts
 
 
 if __name__ == '__main__':
