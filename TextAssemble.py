f81bc57233f81b0c64cfb5fb295bcd1b4e4069d6
Yohannes <yohannes.af@gmail.com>
2016-11-21 10:30:42 -0800

    Syl colors added

--- TextAssemble.py
+++ TextAssemble.py
@@ -5,6 +5,7 @@ from finnsyll import FinnSyll
 import pronouncing as pr
 from syllabify.syllabify import syllabify as syl
 from hyphen import Hyphenator, dict_info
+from colorama import *
 import pandas as pd
 import numpy as np
 
@@ -18,14 +19,15 @@ class TextAssemble(ScoreMechanism):
         ScoreMechanism.__init__(self, filepath)
         self.M_output = None
         self.clusters = defaultdict()
-        # self.clustered_syl = OrderedDict()
+        self.unique_clusters = 0
         self.clustered_syl = []
         self.grouped_syl = OrderedDict()
+        self.colored_syl = OrderedDict()
 
         self.mcl_cluster()
-        self.phone_name_assignment()
+        self.word_name_assignment()
         self.syl_combine()
-
+        self.color_assignment()
 
     def mcl_cluster(self):
         '''
@@ -34,19 +36,18 @@ class TextAssemble(ScoreMechanism):
         self.M_output, self.clusters = mcl(self.adjacency_matrix,
                           expand_factor = 7,
                           inflate_factor = 3,
-                          max_loop = 1000,
-                          mult_factor = 4
+                          max_loop = 100,
+                        #   mult_factor = 4
                           )
 
-    def phone_name_assignment(self):
+    def word_name_assignment(self):
         '''
         Assigns the word syllables the approprate cluster
         '''
         temp = self.cluster_val_inversion()
-        # test = []
+        self.unique_clusters = list(set(temp.values()))
         for syl, cl in temp.iteritems():
             self.clustered_syl.append([self.word_syl_col[syl], cl])
-            # self.clustered_syl.update({self.word_syl_col[syl]:cl})
 
     def cluster_val_inversion(self):
         '''
@@ -78,7 +79,21 @@ class TextAssemble(ScoreMechanism):
             syl_counts.update({key:len(val)})
         return syl_counts
 
+    def color_assignment(self):
+        black = Fore.BLACK
+        colors = [Fore.RED, Fore.BLUE, Fore.CYAN, Fore.GREEN, Fore.YELLOW, Fore.MAGENTA]
+        num_clusters = len(colors) + 1
+        for key, syl in self.grouped_syl.iteritems():
+            temp = []
+            for sound in syl:
+                ph, cl = sound[0], sound[1]
+                if cl == 99:
+                    temp.append(black + ph)
+                else:
+                    color_ind = self.unique_clusters.index(cl)
+                    temp.append(colors[color_ind] + ph)
+            self.colored_syl.update({key:temp})
 
 if __name__ == '__main__':
 
-    text = TextAssemble('lyrics/mini.md')
+    text = TextAssemble('lyrics/forgot.md')
